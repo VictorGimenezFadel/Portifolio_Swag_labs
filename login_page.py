3e9480cb3dbc3ec3cea9296e4897762a75026253
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-24 16:29:26 -0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,32 +1,25 @@
 """
-Login do Swag Labs
-
-TIME SLEEP É SOMENTE PARA UMA VISUALIZAÇÃO QUE SIMULA UMA NAVEGAÇÃO DE UM USUÁRIO
+ORGANIZAÇÃO DA login_page - COMANDOS DE LOGIN
+login_page.py
 """
-
-import time
 from selenium.webdriver.common.by import By
+from BasePage import CL_BasePage
 
 
-class LoginPage:
+class CL_LoginPage(CL_BasePage):
 
     def __init__(self, browser):
-        self.browser = browser
+        super().__init__(browser) # Necessário para inicializar corretamente a BasePage
+        self.campo_username = (By.ID, "user-name")
+        self.campo_password = (By.ID, "password")
+        self.btn_login = (By.ID, "login-button")
 
     def fazer_login(self, usuario, senha):
+        # Interação com o Campo de username
+        self.escrever(self.campo_username, usuario) # Escrever no campo de usuário, um usuário válido
 
-        #Interação com o Campo de username
-        username = self.browser.find_element(By.ID, "user-name")
-        username.send_keys(usuario)
-        time.sleep(1)
+        # Interação com o Campo de password
+        self.escrever(self.campo_password, senha) # Escrever no campo de senha, uma senha válida
 
-        #Interação com o Campo de password
-        password = self.browser.find_element(By.ID, "password")
-        password.send_keys(senha)
-        time.sleep(1)
-
-        #interação com o Botão de login
-        btn_login = self.browser.find_element(By.ID, "login-button")
-        btn_login.click()
-        time.sleep(1)
-      
+        # interação com o Botão de login
+        self.clicar(self.btn_login) # Clicar no botão de login
