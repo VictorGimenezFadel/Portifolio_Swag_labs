3f85d1d8b639cd142291a9ca8fdd031a3096f5e7
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-24 16:58:37 -0300

    Update BasePage.py

--- BasePage.py
+++ BasePage.py
@@ -1,7 +1,10 @@
 """"
-SEÇÃO 07 - CRIANDO BasePage – PRIMEIROS MÉTODOS GENERICOS
 BasePage.py
 """
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class CL_BasePage:
     def __init__(self, browser):
         self.browser = browser
@@ -9,11 +12,14 @@ class CL_BasePage:
     def encontrar_elemento(self, locator):
         return self.browser.find_element(*locator)
 
+
     def escrever(self, locator, text):
         self.encontrar_elemento(locator).send_keys(text)
 
+
     def clicar(self, locator):
-        self.encontrar_elemento(locator).click()
+        WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(locator)).click()
+
 
     def verificar_se_elemento_existe(self, locator):
         assert self.encontrar_elemento(locator).is_displayed(), f"O elemento '{locator}' não apareceu na tela no tempo esperado"
