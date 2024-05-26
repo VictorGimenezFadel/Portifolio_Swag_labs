142de9ab4682642475f4b89e790ea6af50736dc0
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-26 11:35:29 -0300

    Create about_page.py

---
+++ about_page.py
@@ -0,0 +1,25 @@
+""""
+Interações / Configurações da página Home
+"""""
+from base_page import BasePage
+from Testes.conftest import browser
+from selenium.webdriver.common.by import By
+
+
+class CL_VerificarPopUp(BasePage):
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.popup_pag_about = (By.CLASS_NAME, "ot-sdk-row") #Verifica se o pop-up está visivel e pode ser clicavel
+
+    def verificar_popup_pag_about(self):
+        self.verificar_se_elemento_existe(self.popup_pag_about) # Verificar se o popup está presente antes fechar ele
+
+
+class CL_FecharPopUp(BasePage):
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.popup_pag_about_fechar = (By.ID, "onetrust-accept-btn-handler")  # Verifica se o pop-up está visivel e pode ser clicavel
+
+    def fechar_popup_pag_about(self):
+        self.clicar(self.popup_pag_about_fechar)
+      
