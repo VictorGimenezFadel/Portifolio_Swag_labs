9ed097d327f32fa624608dcef7f7e1530688edea
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-28 17:58:55 -0300

    Update base_page.py

--- base_page.py
+++ base_page.py
@@ -1,5 +1,5 @@
 """"
-Métodos reutilizaveis
+Métodos reutilizáveis
 """
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,6 +13,10 @@ class BasePage:
         return self.browser.find_element(*locator)
 
 
+    def encontrar_elementos(self, locator):
+        return self.browser.find_elements(*locator)
+
+
     def escrever(self, locator, text):
         self.encontrar_elemento(locator).send_keys(text)
 
@@ -24,3 +28,15 @@ class BasePage:
     def verificar_se_elemento_existe(self, locator):
         assert self.encontrar_elemento(locator).is_displayed(), f"O elemento '{locator}' não apareceu na tela no tempo esperado"
                                                                 # Mensagem para caso o elemento não esteja presente
+
+
+    def verificar_se_elementos_existem(self, locator): # Usar somente em situações pontuais onde aparecem varios elementos com um mesmo locator
+        assert self.encontrar_elementos(locator).is_displayed(), f"Os elementos '{locator}' não aparecem na tela no tempo esperado"
+
+
+    def voltar_pag_anterior(self):
+        self.browser.back()
+
+
+    def voltar_pag_que_estava(self):
+        self.browser.forward()
