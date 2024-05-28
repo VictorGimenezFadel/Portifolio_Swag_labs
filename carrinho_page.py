d4513aa2c8239ca85550f7a277f31fc33820b0da
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-28 19:53:23 -0300

    Update carrinho_page.py

--- carrinho_page.py
+++ carrinho_page.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 
 # 1° acesso a Página Carrinho
-class CL_IdentificarItens_carrinho(BasePage):
+class CL_IdentificarItens_carrinho_01(BasePage): # 01 = 1° Identificação de itens dentro do carrinho
     def __init__(self, browser):
         super().__init__(browser)
         self.btn_remover_item_bike_light = (By.ID, "remove-sauce-labs-backpack")
@@ -15,7 +15,7 @@ class CL_IdentificarItens_carrinho(BasePage):
 
 
 
-class CL_RemoverItem_carrinho(BasePage):
-    def remover_item_carrinho(self,identificar_itens):
-        self.clicar(identificar_itens.btn_remover_item_bike_light)
-        self.clicar(identificar_itens.btn_remover_item_onesie)
+class CL_RemoverItem_carrinho_01(BasePage): # 01 = 1° Remoção de itens dentro do carrinho
+    def remover_item_carrinho_01(self,remover_item):
+        self.clicar(remover_item.btn_remover_item_bike_light)
+        self.clicar(remover_item.btn_remover_item_onesie)
