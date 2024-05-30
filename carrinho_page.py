5a6e9fd2a10fd912161079184e96d31cee8c4719
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-30 10:31:41 -0300

    Update carrinho_page.py

--- carrinho_page.py
+++ carrinho_page.py
@@ -1,25 +1,23 @@
 """"
 Interações / Configurações da página Carrinho
 """""
-from base_page import BasePage
-from Testes.conftest import browser
+from Pages.base_page import BasePage
+from conftest import browser
 from selenium.webdriver.common.by import By
 
 
 # 1° acesso a Página Carrinho
-class CL_IdentificarItens_carrinho_01(BasePage): # 01 = 1° Identificação de itens dentro do carrinho
+class CL_IdentificarItens_carrinho_01(BasePage):  # 01 = 1° Identificação de itens dentro do carrinho
     def __init__(self, browser):
         super().__init__(browser)
         self.btn_remover_item_bike_light = (By.ID, "remove-sauce-labs-backpack")
         self.btn_remover_item_onesie = (By.ID, "remove-sauce-labs-onesie")
 
 
-
-class CL_RemoverItem_carrinho_01(BasePage): # 01 = 1° Remoção de itens dentro do carrinho
-    def remover_item_carrinho_01(self,remover_item):
+class CL_RemoverItem_carrinho_01(BasePage):  # 01 = 1° Remoção de itens dentro do carrinho
+    def remover_item_carrinho_01(self, remover_item):
         self.clicar(remover_item.btn_remover_item_bike_light)
         self.clicar(remover_item.btn_remover_item_onesie)
-        
 
 
 class CL_IdentificarBtn_checkout(BasePage):
@@ -28,8 +26,6 @@ class CL_IdentificarBtn_checkout(BasePage):
         self.btn_checkout = (By.ID, "checkout")
 
 
-
 class CL_RealizarCheckout(BasePage):
     def realizar_checkout_carrinho(self, realizar_checkout):
-    self.clicar(realizar_checkout.btn_checkout)
-    
+        self.clicar(realizar_checkout.btn_checkout)
