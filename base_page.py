53911a5d10a3536234d18e13bf9cffa001a4d3bd
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-05-30 10:31:25 -0300

    Update base_page.py

--- base_page.py
+++ base_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class BasePage:
+class BasePage():
     def __init__(self, browser):
         self.browser = browser
 
