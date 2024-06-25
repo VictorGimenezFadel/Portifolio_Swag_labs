0434b10f2daac5a5446c5a9d20fc8676a307d39e
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-06-25 12:16:26 -0300

    Update 03-demais_metodos.py

--- 03-demais_metodos.py
+++ 03-demais_metodos.py
@@ -1,6 +1,9 @@
 """
 Essa arquivo serve para métodos alternativos para uma mesma funcionalidade
 """
+
+# O TRECHO DESSE MÉTODO SERVE PARA REALIZAR A VERIFICAÇÃO DE MULTIPLOS ITENS
+
 from selenium.webdriver.common.by import By
 from base_page import BasePage
 
@@ -19,3 +22,18 @@ class Cl_VerificarElementos_ItensCarrinho_Verif01(BasePage):
             (By.XPATH, "//a[@href='#' and @id='item_2_title_link']"),
         ]:
             assert self.browser.find_element(*item_locator).is_displayed()
+
+
+
+
+
+# ESSE MÉTODO SERVE PARA DINAMIZAR A ADIÇÃO DE ITENS AO CARRINHO ATRAVÉS DO TEXTO (TITULO DO PRODUTO)
+
+class CL_AdicionarItens_carrinho(BasePage):  # 01 = 1° Identificação de itens para o carrinho
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.item_inventario = (By.XPATH, "//*[@class='inventory_item_name ' and text()='{}']")
+        self.carrinho = (By.XPATH, "//*[text()='Add to cart']")
+
+    def add_ao_carrinho(self, nome_item):
+        item = (self.item_inventario[0], self.item_inventario[1])
