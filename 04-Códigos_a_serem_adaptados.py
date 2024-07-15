b7ceed1d60f85d279cfcb6d0f2b47fbfe834351a
Victor Gimenez Fadel <69645306+VictorGimenezFadel@users.noreply.github.com>
2024-07-15 15:05:33 -0300

    Update 04-Códigos_a_serem_adaptados.py

--- 04-Códigos_a_serem_adaptados.py
+++ 04-Códigos_a_serem_adaptados.py
@@ -11,20 +11,3 @@
         optn_z_a = WebDriverWait(browser, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//option[@value='za']")))
         optn_z_a.click()
-
-
-
-        #Segunda interação com o Menu lateral (3 risquinhos)
-        menu_lateral_esq = WebDriverWait(browser, 30).until(
-            EC.element_to_be_clickable((By.ID, "react-burger-menu-btn")))
-        menu_lateral_esq.click()
-
-
-
-        #Opção About (Sobre)
-        optn_about = WebDriverWait(browser, 30).until(
-            EC.element_to_be_clickable(By.XPATH, "//a[@id='about_sidebar_link']"))
-        optn_about.click()
-
-
-
